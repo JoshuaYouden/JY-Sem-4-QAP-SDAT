32cad2da86b289fa7df17cd4023cf68c10dd9604
Joshua Youden <Joshua.Youden@keyin.com>
2025-03-16 12:51:38 -0230

    Added code to other half of files in tournaments

--- src/main/java/com/keyin/rest/tournaments/TournamentsController.java
+++ src/main/java/com/keyin/rest/tournaments/TournamentsController.java
@@ -1,4 +1,47 @@
 package com.keyin.rest.tournaments;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+@RequestMapping("/api")
+@RestController
+@CrossOrigin
 public class TournamentsController {
+    @Autowired
+    private TournamentsService tournamentsService;
+
+    @GetMapping("/tournaments")
+    public List<Tournaments> getAllTournaments() {
+        return tournamentsService.findAllTournaments();
+    }
+
+    @GetMapping("/tournaments/{id}")
+    public Tournaments getTournamentById(@PathVariable(value = "id") long id) {
+        return tournamentsService.findTournamentById(id);
+    }
+
+    @PostMapping("/tournaments_search")
+    public List<Tournaments> searchTournaments(@RequestBody Tournaments tournaments) {
+        List<Tournaments> results = new ArrayList<Tournaments>();
+
+        Tournaments tournament = tournamentsService.findTournamentByLocation(tournaments.getTournamentLocation());
+
+        if (tournament != null) {
+            results.add(tournament);
+        }
+
+        return results;
+    }
+
+    @PostMapping("/tournaments")
+    public Tournaments createTournament(@RequestBody Tournaments newTournament) {
+        return tournamentsService.createTournament(newTournament);
+    }
+
+    @PutMapping("/tournaments/{id}")
+    public Tournaments updateTournament(@PathVariable(value = "id") long id, @RequestBody Tournaments updatedTournament) {
+        return tournamentsService.updateTournament(id, updatedTournament);
+    }
 }
--- src/main/java/com/keyin/rest/tournaments/TournamentsService.java
+++ src/main/java/com/keyin/rest/tournaments/TournamentsService.java
@@ -1,4 +1,48 @@
 package com.keyin.rest.tournaments;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TournamentsService {
+    @Autowired
+    private TournamentsRepository tournamentsRepository;
+
+    public List<Tournaments> findAllTournaments() {
+        return (List<Tournaments>) tournamentsRepository.findAll();
+    }
+
+    public Tournaments findTournamentById(long id) {
+        Optional<Tournaments> optionalTournaments = tournamentsRepository.findById(id);
+
+        return optionalTournaments.orElse(null);
+    }
+
+    public Tournaments findTournamentByLocation(String tournamentLocation) {
+        return tournamentsRepository.findByTournamentLocation(tournamentLocation);
+    }
+
+    public Tournaments createTournament(Tournaments newTournament) {
+        return tournamentsRepository.save(newTournament);
+    }
+
+    public Tournaments updateTournament(long id, Tournaments updatedTournament) {
+        Tournaments tournamentToUpdate = findTournamentById(id);
+
+        if (tournamentToUpdate != null) {
+            tournamentToUpdate.setTournamentLocation(updatedTournament.getTournamentLocation());
+            tournamentToUpdate.setTournamentStartDate(updatedTournament.getTournamentStartDate());
+            tournamentToUpdate.setTournamentEndDate(updatedTournament.getTournamentEndDate());
+            tournamentToUpdate.setTournamentEntryFee(updatedTournament.getTournamentEntryFee());
+            tournamentToUpdate.setTournamentCashPrize(updatedTournament.getTournamentCashPrize());
+            tournamentToUpdate.setParticipatingMembers(updatedTournament.getParticipatingMembers());
+
+            return tournamentsRepository.save(tournamentToUpdate);
+        }
+
+        return null;
+    }
 }
